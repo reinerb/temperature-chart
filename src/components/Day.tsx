e38c5a4a6f89b09f823a220707c74706eeac3209
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-08-03 15:05:35 -0400

    Add styles for Day component

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -5,15 +5,41 @@ interface Props {
   weekday: number;
 }
 
+const getTemperatureStyles = (temperature: number) => {
+  if (temperature >= 100) {
+    return "bg-red-900 text-white";
+  } else if (temperature >= 90) {
+    return "bg-red-600 text-white";
+  } else if (temperature >= 80) {
+    return "bg-orange-600 text-white";
+  } else if (temperature >= 70) {
+    return "bg-yellow-500 text-black";
+  } else if (temperature >= 60) {
+    return "bg-green-800 text-white";
+  } else if (temperature >= 50) {
+    return "bg-teal-600 text-white";
+  } else if (temperature >= 33) {
+    return "bg-blue-600 text-white";
+  } else if (temperature >= 16) {
+    return "bg-violet-700 text-white";
+  } else {
+    return "bg-purple-600 text-white";
+  }
+};
+
 function Day({ day, temperature, celsius, weekday }: Props) {
   return (
     <div
-      className={`flex h-14 w-14 flex-col items-center justify-center bg-slate-900 text-white ${
+      className={`h-14 w-14  ${getTemperatureStyles(temperature)} text-white ${
         day === 1 && `col-start-${weekday}`
       }`}
     >
-      <p>{day}</p>
-      <p>{`${temperature}°${celsius ? "C" : "F"}`}</p>
+      <div className="m-auto flex cursor-pointer flex-col items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
+        <p className="text-xl">{day}</p>
+        <p className="text-l font-bold">{`${temperature}°${
+          celsius ? "C" : "F"
+        }`}</p>
+      </div>
     </div>
   );
 }
